69673c4b25ac3b87fdcbdb53a96c7a631c55f354
Marin Atanasov Nikolov <dnaeon@gmail.com>
2019-02-06 11:50:44 +0200

    CLJ-2477: Use consistent parameter naming in clojure.core/assoc

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -179,18 +179,18 @@
  vector? (fn ^:static vector? [x] (instance? clojure.lang.IPersistentVector x)))
 
 (def
- ^{:arglists '([map key val] [map key val & kvs])
+ ^{:arglists '([m k v] [m k v & kvs])
    :doc "assoc[iate]. When applied to a map, returns a new map of the
     same (hashed/sorted) type, that contains the mapping of key(s) to
     val(s). When applied to a vector, returns a new vector that
-    contains val at index. Note - index must be <= (count vector)."
+    contains v at index. Note - index must be <= (count vector)."
    :added "1.0"
    :static true}
  assoc
  (fn ^:static assoc
-   ([map key val] (clojure.lang.RT/assoc map key val))
-   ([map key val & kvs]
-    (let [ret (clojure.lang.RT/assoc map key val)]
+   ([m k v] (clojure.lang.RT/assoc m k v))
+   ([m k v & kvs]
+    (let [ret (clojure.lang.RT/assoc m k v)]
       (if kvs
         (if (next kvs)
           (recur ret (first kvs) (second kvs) (nnext kvs))
